8805e32b78350ab7c8fd95e1f988ded7e823a01a
vancomm <peshekhonov.ip@gmail.com>
2023-03-04 18:15:13 +0300

    Overhauled popup structure; implemented more precise crosshair adjustment

--- src/content-main.js
+++ src/content-main.js
@@ -1,103 +1,152 @@
+/* eslint-disable no-unused-vars */
 import { makeStore } from './common.js';
 
-const className = 'crosshair';
+const CH_CLASSNAME = 'crosshair';
 
-function removeCrosshair() {
-    const targets = document.getElementsByClassName(className);
-    [...targets].forEach((target) => target.remove());
-}
+function render({ 
+    show, dot, lines,
+    thickness, length,
+    invert, color, opacity, 
+    absolute, x, y 
+}) {
+    const staleElements = document.getElementsByClassName(CH_CLASSNAME);
+    [...staleElements].forEach((el) => el.remove());
 
-function render({ show, inverse, color, opacity, size, absolute, x, y }) {
-    removeCrosshair();
-    if (!show) {
-        console.log('not rendering crosshair');
+    if (!show || !(dot || lines)) {
         return;
     }
-    console.log('rendering crosshair');
-    const ch = document.createElement('div');
 
-    ch.className = className;
+    /** @param {HTMLElement} el */
+    const setColor = (el) => {
+        el.style.background = color;
+        el.style.opacity = `${opacity}%`;
 
-    ch.style.position = 'fixed';
-    ch.style.top = '50%';
-    ch.style.left = '50%';
-    ch.style.zIndex = '9999999';
+        if (invert) {
+            el.style.background = 'transparent';
+            el.style.backdropFilter = 'invert(100%)';
+        }
+    };
 
-    ch.style.width = `${size}px`;
-    ch.style.aspectRatio = '1 / 1';
+    const container = document.createElement('div');
 
-    ch.style.translate = '-50% -50%';
+    container.className = CH_CLASSNAME;
 
-    ch.style.background = color;
-    ch.style.opacity = (opacity / 100).toFixed(2);
-
-    if (inverse) {
-        ch.style.background = 'white';
-        ch.style.mixBlendMode = 'difference';
-    }
+    container.style.position = 'fixed';
+    container.style.top = '50%';
+    container.style.left = '50%';
 
     if (absolute) {
-        ch.style.top = `${y}px`;
-        ch.style.left = `${x}px`;
+        container.style.top = `${y}px`;
+        container.style.left = `${x}px`;
     }
 
-    document.body.appendChild(ch);
+    container.style.zIndex = '9999999';
+
+    container.style.width = `${thickness}px`;
+    container.style.aspectRatio = '1 / 1';
+
+    container.style.translate = '-50% -50%';
+
+    container.style.background = 'transparent';
+
+    if (lines) {
+        const lines = Array.from(
+            { length: 4 }, 
+            () => document.createElement('div')
+        );
+        
+        lines[0].style.top = '0';
+        lines[0].style.left = '100%';
+        lines[0].style.width = `${length}px`;
+        lines[0].style.height = `${thickness}px`;
+
+        lines[1].style.bottom = '100%';
+        lines[1].style.left = '0';
+        lines[1].style.width = `${thickness}px`;
+        lines[1].style.height = `${length}px`;
+
+        lines[2].style.top = '0';
+        lines[2].style.right = '100%';
+        lines[2].style.width = `${length}px`;
+        lines[2].style.height = `${thickness}px`;
+
+        lines[3].style.top = '100%';
+        lines[3].style.left = '0';
+        lines[3].style.width = `${thickness}px`;
+        lines[3].style.height = `${length}px`;
+
+        lines.forEach((line) => {
+            setColor(line);
+            line.style.position = 'absolute';
+            container.appendChild(line);
+        });
+    }
+
+    if (dot) {
+        const dotEl = document.createElement('div');
+
+        dotEl.className = CH_CLASSNAME;
+
+        dotEl.style.position = 'absolute';
+        dotEl.style.top = '0';
+        dotEl.style.left = '0';
+
+        dotEl.style.width = `${thickness}px`;
+        dotEl.style.aspectRatio = '1 / 1';
+        
+        setColor(dotEl);
+
+        container.appendChild(dotEl);
+    }
+
+    document.body.appendChild(container);
 } 
 
 export async function main() {
     const storeName = window.location.href.split(/(?<![/:])\//, 1)[0];
-    
     const store = makeStore(storeName);
 
     const cache = await store.get(null);
-
-    const state = new Proxy({}, { 
+    const state = new Proxy(cache, { 
         set: (target, key, value) => {
             const oldValue = target[key];
-            if (oldValue === value) {
-                return true;
+            if (oldValue !== value) {
+                target[key] = value;
+                render(target);
             }
-            target[key] = value;
-            render(target);
             return true;
         }
     });
+
+    render(state);
 
     /**
      * @this {HTMLElement}
      * @param {MouseEvent} e 
      */
-    function handlePlaceWithMouse(e) {
+    const handlePlaceWithMouse = function (e) {
         e.preventDefault();
         e.stopImmediatePropagation();
         store.set({ 
             x: e.clientX, 
             y: e.clientY,
-            isPlacing: false,
+            place: false,
         });
-        console.log('finished placing');
-        this.removeEventListener('click', handlePlaceWithMouse, true);
+    };
+
+    if (state.place) {
+        window.addEventListener('click', handlePlaceWithMouse, { capture: true, once: true });
     }
     
-    for (const key in cache) {
-        console.log(`retrieved cached value of ${key}: ${JSON.stringify(cache[key])}`);
-        state[key] = cache[key];
-    }
-
-    if (state.isPlacing) {
-        window.addEventListener('click', handlePlaceWithMouse, true);
-    }
-
     store.addListener((changes) => {
         if (typeof changes.newValue !== 'object') {
             return;
         }
         for (const key in changes.newValue) {
-            console.log(`detected change ${key}: ${JSON.stringify(changes.newValue[key])}`);
             state[key] = changes.newValue[key];
         }
-        if (state.isPlacing) {
-            window.addEventListener('click', handlePlaceWithMouse, true);
+        if (state.place) {
+            window.addEventListener('click', handlePlaceWithMouse, { capture: true, once: true });
         }
     });
 }
--- src/popup.js
+++ src/popup.js
@@ -1,8 +1,33 @@
 /* eslint-disable no-unused-vars */
 import { makeStore } from './common.js';
 
-const MAX_CROSSHAIR_SIZE = 9999;
-const CROSSHAIR_SIZE_INCREMENT = 2;
+/** @type {Array<HTMLInputElement>} */
+const inputs = [...document.querySelectorAll('.settings input')];
+
+/**
+ * @param {Array<HTMLInputElement>} inputs 
+ * @param {string} key
+ * @returns {Object<string, any>}
+ */
+function extractDataAttributes(inputs, key) {
+    return inputs.reduce((acc, input) => {
+        if (!(key in input.dataset)) {
+            return acc;
+        }
+        let value = input.dataset[key];
+        if (input.type === 'checkbox') {
+            value = value === 'true' ? true : false;
+        }
+        return { [input.id]: value, ...acc };
+    }, {});
+}
+
+const defaultSettings = extractDataAttributes(inputs, 'defaultValue');
+
+const initialSettings = { 
+    ...extractDataAttributes(inputs, 'initialValue'), 
+    ...defaultSettings
+};
 
 /** @returns {Promise<string|void>} */
 async function queryCurrentURL() {
@@ -13,182 +38,95 @@ async function queryCurrentURL() {
     return tabs[0].url;
 }
 
-const defaultSettings = {
-    inverse: false,
-    color: '#ff0000',
-    opacity: '100',
-    size: '6',
-    absolute: false,
-    x: '50',
-    y: '50',
-    isPlacing: false,
-};
-
-const initialSettings = {
-    show: false,
-    ...defaultSettings,
-};
-
-const inputProps = [
-    {
-        id: 'show',
-        type: 'checkbox',
-    },
-    {
-        id: 'inverse',
-        type: 'checkbox'
-    },
-    {
-        id: 'color',
-        type: 'color'
-    },
-    {
-        id: 'opacity',
-        type: 'number',
-        max: '100',
-    },
-    {
-        id: 'size',
-        type: 'number',
-        max: MAX_CROSSHAIR_SIZE,
-        increment: CROSSHAIR_SIZE_INCREMENT,
-    },
-    {
-        id: 'absolute',
-        type: 'checkbox'
-    },
-    {
-        id: 'x',
-        type: 'number',
-    },
-    {
-        id: 'y',
-        type: 'number',
-    },
-];
-
 async function app() {
     if (!chrome) {
         return;
     }
 
     const url = await queryCurrentURL();
+
     if (!url || !url.startsWith('http')) {
         const veil = document.getElementById('veil');
-        veil.style = '';
+        veil.dataset.show = 'true';
         return;
     }
 
     const storeName = url.split(/(?<![/:])\//, 1)[0];
-
     const store = makeStore(storeName);
-
-    /** @param {string} id */
-    const makeWheelHandler = (id, { increment = 2, min = 0, max = Infinity } = {}) =>
-        /**
-         * @this {GlobalEventHandlers}
-         * @param {WheelEvent} e
-         */
-        async function handleWheel(e) {
-            const delta = increment * -1 * Math.sign(e.deltaY);
-            let value = await store.get(id);
-            value = Number(value);
-            value = value + delta;
-            value = Math.min(Math.max(value, min), max);
-            value = value.toString();
-            this.value = value;
-            store.set({ [id]: value });
-        };
     
-
-    /** @param {string} id */
-    const makeNumberInputHandler = (id, { min = 0, max = Infinity } = {}) => 
-        /** @this {GlobalEventHandlers} */
-        async function handleNumberInput() {
-            let value = this.value.replace(/\D/g, '').replace(/(?<=^)0(?=\d)/, '');
-            value = Number(value);
-            value = Math.min(Math.max(value, min), max);
-            value = value.toString();
-            this.value = value;
-            store.set({ [id]: value });
-        };
-    
-
     const cache = await store.get(null);
-
-    const settings =
-    typeof cache !== 'object'
+    const settings = typeof cache !== 'object'
         ? initialSettings
         : { ...initialSettings, ...cache };
 
     await store.set(settings);
 
-    /** @type {HTMLInputElement} */
-    const showCheckbox = document.getElementById('show');
-    showCheckbox.checked = settings.show;
-    showCheckbox.onchange = function () { store.set({ show: this.checked }); };
+    /** @param {string} storeId */
+    const makeNumberInputHandler = (storeId, { min = 0, max = Infinity } = {}) => 
+        /** @this {GlobalEventHandlers} */
+        async function handleNumberInput() {
+            min = Number(min);
+            max = Number(max);
+            let value = this.value.replace(/\D/g, '').replace(/(?<=^)0(?=\d)/, '');
+            value = Number(value);
+            value = Math.min(Math.max(value, min), max);
+            value = value.toString();
+            this.value = value;
+            store.set({ [storeId]: value });
+        };
 
-    /** @type {HTMLInputElement} */
-    const inverseCheckbox = document.getElementById('inverse');
-    inverseCheckbox.checked = settings.inverse;
-    inverseCheckbox.onchange = function () { store.set({ inverse: this.checked }); };
+    /** @param {string} storeId */
+    const makeWheelHandler = (storeId, { step = 2, min = 0, max = Infinity } = {}) =>
+        /**
+         * @this {GlobalEventHandlers}
+         * @param {WheelEvent} e
+         */
+        async function handleWheel(e) {
+            min = Number(min);
+            max = Number(max);
+            step = Number(step);
+            const delta = step * -1 * Math.sign(e.deltaY);
+            let value = await store.get(storeId);
+            value = Number(value);
+            console.log(`on wheel: ${value+delta}/${max}`);
+            value = value + delta;
+            value = Math.min(Math.max(value, min), max);
+            value = value.toString();
+            this.value = value;
+            store.set({ [storeId]: value });
+        };
 
-    /** @type {HTMLInputElement} */
-    const colorInput = document.getElementById('color');
-    colorInput.checked = settings.color;
-    colorInput.onchange = function () { store.set({ color: this.value }); };
-
-    /** @type {HTMLButtonElement} */
-    const opacityInput = document.getElementById('opacity');
-    opacityInput.value = settings.opacity;
-    opacityInput.oninput = makeNumberInputHandler('opacity', { max: 100, min: 0 });
-    opacityInput.onwheel = makeWheelHandler('opacity', { max: 100, min: 0 });
-
-    /** @type {HTMLInputElement} */
-    const sizeInput = document.getElementById('size');
-    sizeInput.value = settings.size;
-    sizeInput.oninput = makeNumberInputHandler('size', { max: MAX_CROSSHAIR_SIZE });
-    sizeInput.onwheel = makeWheelHandler('size', {
-        increment: CROSSHAIR_SIZE_INCREMENT,
-        max: MAX_CROSSHAIR_SIZE,
+    inputs.forEach((input) => {
+        const { id, type, } = input;
+        switch (type) {
+        case 'number':
+            input.value = settings[id];
+            input.oninput = makeNumberInputHandler(id, input);
+            input.onwheel = makeWheelHandler(id, input);
+            break;
+        case 'color':
+        case 'text':
+            input.value = settings[id];
+            input.oninput = function handleInput() { 
+                store.set({ [id]: this.value }); 
+            };
+            break;
+        case 'checkbox':
+            input.checked = settings[id];
+            input.onchange = function handleInput() { 
+                store.set({ [id]: this.checked }); 
+            };
+            break;
+        }
     });
-
-    /** @type {HTMLInputElement} */
-    const absoluteCheckbox = document.getElementById('absolute');
-    absoluteCheckbox.checked = settings.absolute;
-    absoluteCheckbox.onchange = function () { store.set({ absolute: this.checked }); };
-
-    /** @type {HTMLInputElement} */
-    const xInput = document.getElementById('x');
-    xInput.value = settings.x;
-    xInput.oninput = makeNumberInputHandler('x');
-    xInput.onwheel = makeWheelHandler('x', { increment: 2 });
-
-    /** @type {HTMLInputElement} */
-    const yInput = document.getElementById('y');
-    yInput.value = settings.y;
-    yInput.oninput = makeNumberInputHandler('y');
-    yInput.onwheel = makeWheelHandler('y', { increment: 2 });
-
-    /** @type {HTMLInputElement} */
-    const placingToggle = document.getElementById('is-placing');
-    placingToggle.checked = settings.isPlacing;
-    placingToggle.onchange = function () { store.set({ isPlacing: this.checked }); };
-
-    /** @type {HTMLButtonElement} */
-    const restoreButton = document.getElementById('restore');
-    restoreButton.onclick = function () {
-        store.set(defaultSettings);
-    };
 
     store.addListener((changes) => {
         console.log(`received changes: ${JSON.stringify(changes)}`);
-        inputProps.forEach(({ id, type }) => {
+        inputs.forEach((input) => {
+            const { id, type } = input;
             if (!(id in changes.newValue)) {
                 return;
             }
-            /** @type {HTMLInputElement} */
-            const input = document.getElementById(id);
             const update = changes.newValue[id];
             if (input.value === update) {
                 return;
@@ -204,6 +142,12 @@ async function app() {
             }
         });
     });
+
+    /** @type {HTMLButtonElement} */
+    const restoreButton = document.getElementById('restore');
+    restoreButton.onclick = function () {
+        store.set(defaultSettings);
+    };
 }
 
 app();
